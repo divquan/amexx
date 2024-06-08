4e4567696831580cde9fcb31f772ca908217f248
Divine Quansah <divquan@gmail.com>
2024-06-08 15:53:46 +0000

    Refactor verification page component and remove unused code

--- app/(dashboard)/(auth)/verify/page.tsx
+++ app/(dashboard)/(auth)/verify/page.tsx
@@ -1,38 +1,9 @@
-"use client";
-
-import { account } from "@/lib/appwrite";
 import { Loader2 } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
 
-import React, { Suspense, useEffect } from "react";
-import { toast } from "sonner";
+import React, { Suspense } from "react";
 import VerificationLoader from "./verificationLoading";
 
 const Page = () => {
-  const [state, setState] = React.useState({ loading: true, verified: false });
-  const [error, setError] = React.useState("");
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const secret = searchParams?.get("secret") || "";
-    const userId = searchParams?.get("userId") || "";
-    const promise = account.updateVerification(userId, secret);
-
-    promise.then(
-      function (response) {
-        console.log(response);
-        setState({ loading: false, verified: true });
-        router.push("/dashboard");
-      },
-      function (error) {
-        console.log(error);
-        setState({ loading: false, verified: false });
-        toast.error("Couldn't verify", { style: { backgroundColor: "red" } });
-        setError("Couldn't verify email");
-      }
-    );
-  }, []);
   return (
     <Suspense
       fallback={
