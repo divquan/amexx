125ab1abe3f858d5313f4acbb5bc89e1f21e5e37
divquan <divquan@gmail.com>
2024-08-10 21:36:09 +0000

    fix  jotform height issues

--- app/(landingPages)/apply/page.tsx
+++ app/(landingPages)/apply/page.tsx
@@ -20,16 +20,16 @@ import { useEffect } from "react";
 
 const ApplicationForm = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js";
-    script.onload = () => {
-      // @ts-ignore
-      window.jotformEmbedHandler(
-        "iframe[id='JotFormIFrame-242187298929070']",
-        "https://form.jotform.com/"
-      );
-    };
-    document.body.appendChild(script);
+    // const script = document.createElement("script");
+    // script.src = "https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js";
+    // script.onload = () => {
+    //   // @ts-ignore
+    //   window.jotformEmbedHandler(
+    //     "iframe[id='JotFormIFrame-242187298929070']",
+    //     "https://form.jotform.com/"
+    //   );
+    // };
+    // document.body.appendChild(script);
   }, []);
 
   return (
@@ -44,10 +44,10 @@ const ApplicationForm = () => {
       style={{
         minWidth: "100%",
         maxWidth: "100%",
-        height: "539px",
+        height: "1500px",
         border: "none",
         paddingTop: "100px",
-        paddingBottom: "40px",
+        // paddingBottom: "40px",
       }}
       scrolling="no"
     ></iframe>
