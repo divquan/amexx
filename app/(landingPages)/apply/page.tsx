00773e801daa89fdb323fa8cd31951c7d4726dee
divquan <divquan@gmail.com>
2024-08-10 21:50:57 +0000

    add loading icon for jotform form

--- app/(landingPages)/apply/page.tsx
+++ app/(landingPages)/apply/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+import { Loader2 } from "lucide-react";
+import React, { useState } from "react";
 
 const Page = () => {
   return (
@@ -19,6 +20,7 @@ export default Page;
 import { useEffect } from "react";
 
 const ApplicationForm = () => {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     // const script = document.createElement("script");
     // script.src = "https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js";
@@ -33,23 +35,33 @@ const ApplicationForm = () => {
   }, []);
 
   return (
-    <iframe
-      id="JotFormIFrame-242187298929070"
-      title="Application Form"
-      onLoad={() => window.parent.scrollTo(0, 0)}
-      allowTransparency={true}
-      allow="geolocation; microphone; camera; fullscreen"
-      src="https://form.jotform.com/213502382215546"
-      frameBorder="0"
-      style={{
-        minWidth: "100%",
-        maxWidth: "100%",
-        height: "1800px",
-        border: "none",
-        paddingTop: "100px",
-        // paddingBottom: "40px",
-      }}
-      scrolling="no"
-    ></iframe>
+    <>
+      <iframe
+        id="JotFormIFrame-242187298929070"
+        title="Application Form"
+        onLoad={() => {
+          setLoading(false);
+          window.parent.scrollTo(0, 0);
+        }}
+        allowTransparency={true}
+        allow="geolocation; microphone; camera; fullscreen"
+        src="https://form.jotform.com/213502382215546"
+        frameBorder="0"
+        style={{
+          minWidth: "100%",
+          maxWidth: "100%",
+          height: "1800px",
+          border: "none",
+          paddingTop: "100px",
+          // paddingBottom: "40px",
+        }}
+        scrolling="no"
+      ></iframe>
+      {loading && (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Loader2 size={42} className="text-primary animate-spin" />
+        </div>
+      )}
+    </>
   );
 };
